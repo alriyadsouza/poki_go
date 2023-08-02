657875b2bb6dc4b303f0404ee7565b301cc9f52f
alriyadsouza <alriyatreeza929@gmail.com>
2023-08-02 21:45:51 +0530

    BAse ui2

--- lib/modules/items/items_page.dart
+++ lib/modules/items/items_page.dart
@@ -110,7 +110,7 @@ class _ItemsPageState extends State<ItemsPage> {
       );
     });
   }
-
+  /*yes*/
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
